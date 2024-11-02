c5dd304fcd170d07730be5acaa1a66b24e408a94
Alex-Pablo <alexpabloanibal@gmail.com>
2024-11-02 08:39:29 -0600

    oka

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.engine('hbs', engine({
   extname: '.hbs',
